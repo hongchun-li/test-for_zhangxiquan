1b4785479b4086bd78c6a794435bf85f91d86f11
zhangxiquanquan <979781924@qq.com>
2026-06-21 19:30:42 +0800

    入库原因: 李洪醇上传的测试命令

---
+++ test_0621_maopao/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+
+    //test printf
+    for(int i = 0;i < 100;i++)
+    {
+        printf("hello word!!!\n");
+        printf("*****************\n");
+        printf("********");
+    }
+    return 0;
+}

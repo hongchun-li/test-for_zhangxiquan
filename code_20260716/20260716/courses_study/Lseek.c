4141a04d1adb8cafdf79824d555e14e905f4f05a
zhangxiquanquan <979781924@qq.com>
2026-07-16 20:07:59 +0800

    入库原因: 代码练习

---
+++ code_20260716/20260716/courses_study/Lseek.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#define N 20
+
+int main(int argc, const char *argv[])
+{
+	int fd = -1;
+	int ret = 0;
+	char buf[N] = {0};
+	if (argc < 2) {
+		printf("Usage : %s <file>\n", argv[0]);
+		return -1;
+	}
+	if ((fd = open(argv[1], O_RDWR|O_CREAT, 0664)) == EOF)
+	{
+		printf("open error\n");
+		return -1;
+	}
+	printf("open success\n");
+
+	ret = lseek(fd, 0, SEEK_CUR);
+	printf("%u\n", ret);
+
+	read(fd, buf, 10);
+	printf("%s", buf);
+	ret = lseek(fd, 0, SEEK_CUR);
+	printf("%u\n", ret);
+
+	lseek(fd, 0, SEEK_END);
+	write(fd, "aaa", strlen("aaa"));
+	ret = lseek(fd, 0, SEEK_CUR);
+	printf("%u\n", ret);
+
+
+	ret = lseek(fd, 0, SEEK_SET);
+	printf("%u\n", ret);
+	bzero(buf, sizeof(buf));
+
+	while( read(fd, buf, sizeof(buf)) > 0) {
+		printf("%s", buf);
+		bzero(buf, sizeof(buf));
+	}
+	printf("Read end\n");
+
+	close(fd);
+	return 0;
+} 
---
+++ code_20260716/20260716/courses_study/Open.c
@@ -0,0 +1,30 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, const char *argv[])
+{
+	int fd = -1;
+#if 1
+	if ((fd = open("3.txt", O_RDONLY)) < 0 )
+	{
+		printf("open error!\n");
+		return -1;
+	}
+	printf("open success!\n");
+#else
+	if ((fd = open("4.txt", O_CREAT|O_RDONLY|O_EXCL, 0664)) < 0)
+	{
+		printf("open error!\n");
+		return -1;
+	}
+	printf("open success!\n");
+		
+#endif
+	if ( !close(fd)) {
+		printf("close success!\n");
+	}
+	return 0;
+}
